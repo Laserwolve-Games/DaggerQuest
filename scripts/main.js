06b824498239c5639e9556c2bbe2cd9786f64fd4
Laserwolve <AndrewRogers@LaserwolveGames.com>
2024-10-07 16:00:27 -0700

    Added passive prototype

--- scripts/main.js
+++ scripts/main.js
@@ -16,9 +16,7 @@ import * as THREE from "three";
 
 import { OrbitControls } from "./three/addons/controls/OrbitControls.js";
 import { RoomEnvironment } from "./three/addons/environments/RoomEnvironment.js";
-
-import { GLTFLoader } from "./three/addons/loaders/GLTFLoader.js";
-import { DRACOLoader } from "./three/addons/loaders/DRACOLoader.js";
+import { OBJLoader } from './three/addons/loaders/OBJLoader.js';
 
 // Three.js objects
 let threeRenderer = null;
@@ -28,44 +26,23 @@ let threeControls = null;
 let threeScene = null;
 
 // Wait for project to start
-runOnStartup(async runtime =>
+runOnStartup(runtime =>
 {
 	runtime.addEventListener("beforeprojectstart", () => OnBeforeProjectStart(runtime));
 });
 
 // When the project starts up, initialize three.js.
 // Note this is asynchronous so the display will remain blank until loading finishes.
-async function OnBeforeProjectStart(runtime)
+function OnBeforeProjectStart(runtime)
 {
-	await InitThreeJs(runtime);
-	
-	// Attach Construct event listeners for handling resize and rendering.
-	runtime.addEventListener("resize", e => OnResize(e));
-	
-	runtime.addEventListener("tick", () => OnTick(runtime));
-}
 
-// Initialize the three.js library.
-async function InitThreeJs(runtime)
-{
-	// Construct's PlatformInfo object provides details about the size of the
-	// main canvas, which is used to get three.js to make a same sized canvas.
 	const platformInfo = runtime.platformInfo;
-	
-	// The HTML element to insert the three.js canvas to is the wrapper element
-	// for HTML layer 0 (i.e. the bottom-most HTML layer, corresponding to the
-	// layer "Back").
 	const container = runtime.getHTMLLayer(0);
-
-	// Create three.js WebGL renderer with antialiasing. Note also that alpha
-	// must be enabled for the three.js canvas to have a transparent background.
 	threeRenderer = new THREE.WebGLRenderer({
 		antialias: true,
 		alpha: true
 	});
-	
-	// Initialize 3D renderer pixel ratio (for high-dpi displays) and size
-	// based on details from Construct's PlatformInfo object.
+
 	threeRenderer.setPixelRatio(platformInfo.devicePixelRatio);
 	threeRenderer.setSize(platformInfo.canvasCssWidth, platformInfo.canvasCssHeight);
 	
@@ -87,44 +64,44 @@ async function InitThreeJs(runtime)
 	threeControls.enablePan = false;
 	threeControls.enableDamping = true;
 
-	// Create a Draco loader for loading 3D models. Point its decoder file path
-	// to the folder in Construct's Files folder.
-	const dracoLoader = new DRACOLoader();
-	dracoLoader.setDecoderPath("draco-gltf-decoder/");
-
-	// Create a GLTF loader to load the sample model with.
-	const loader = new GLTFLoader();
-	loader.setDRACOLoader(dracoLoader);
+	const loader = new OBJLoader();
 	
-	try {
-		// Asyncronously attempt to load the 'Littlest Tokyo' model.
-		// Note this uses a helper function to make the load() method async.
-		const gltf = await LoadGLTF(loader, "models/LittlestTokyo.glb");
-		
-		// Insert the model to the scene and play its animation.
-		const model = gltf.scene;
-		model.position.set(1, 1, 0);
-		model.scale.set(0.01, 0.01, 0.01);
-		threeScene.add(model);
+	// load a resource
+	loader.load(
+		// resource URL
+		'models/passive.obj',
+		// called when resource is loaded
+		function ( object ) {
 
-		threeMixer = new THREE.AnimationMixer(model);
-		threeMixer.clipAction(gltf.animations[0]).play();
-	}
-	catch (err)
-	{
-		console.error("Error loading model: ", err);
-	}
+			// Insert the model to the scene and play its animation.
+			object.position.set(1, 1, 0);
+			object.scale.set(.01, .01, .01);
+			threeScene.add( object );
+			threeMixer = new THREE.AnimationMixer(object);
+			threeMixer.clipAction(object.animations[0]).play();
+
+		},
+		// called when loading is in progresses
+		function ( xhr ) {
+
+			console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+		},
+		// called when loading has errors
+		function ( error ) {
+
+			console.log( 'An error happened' + error.toString());
+
+		}
+	);
+
+	
+	// Attach Construct event listeners for handling resize and rendering.
+	runtime.addEventListener("resize", e => OnResize(e));
+	
+	runtime.addEventListener("tick", () => OnTick(runtime));
 }
 
-// This is a helper method to make the GLTFLoader load() method
-// in to an asyncronous method that can be awaited.
-function LoadGLTF(loader, path)
-{
-	return new Promise((resolve, reject) =>
-	{
-		loader.load(path, resolve, undefined /* progress */, reject);
-	});
-}
 
 // In Construct's resize event, update the 3D renderer size to match.
 function OnResize(e)

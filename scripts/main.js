e3cab1a3cff3507d785622765efb0d60a575659e
Laserwolve <AndrewRogers@LaserwolveGames.com>
2024-10-08 15:52:10 -0700

    removed typescript

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,9 @@
 
-// Use the three.js library for 3D rendering, combining HTML layers to integrate
-// with Construct layers. See: https://threejs.org/
-// This example is based on the following three.js example:
-// https://threejs.org/examples/#webgl_animation_keyframes
+import * as THREE from 'three';
 
-// Notes:
-// - three.js relies on import maps for mapping bare import specifiers like "three".
-//   This project includes an import map (importMap.json) to handle this.
-// - There is some overlap between what three.js does and what Construct does.
-//   For example THREE.Clock can be used in three.js to determine delta-time, but
-//   Construct already calculates this value. Wherever possible, Construct features
-//   are used instead of three.js features for better integration.
-
-import * as THREE from "three";
-
-import { OrbitControls } from "./three/addons/controls/OrbitControls.js";
-import { RoomEnvironment } from "./three/addons/environments/RoomEnvironment.js";
-import { OBJLoader } from './three/addons/loaders/OBJLoader.js';
+import { OrbitControls } from './OrbitControls.js';
+import { RoomEnvironment } from './RoomEnvironment.js';
+import { OBJLoader } from './OBJLoader.js';
 
 // Three.js objects
 let threeRenderer = null;
@@ -28,7 +15,7 @@ let threeScene = null;
 // Wait for project to start
 runOnStartup(runtime =>
 {
-	runtime.addEventListener("beforeprojectstart", () => OnBeforeProjectStart(runtime));
+	runtime.addEventListener('beforeprojectstart', () => OnBeforeProjectStart(runtime));
 });
 
 // When the project starts up, initialize three.js.
@@ -59,7 +46,7 @@ function OnBeforeProjectStart(runtime)
 
 	// Add default OrbitControls allowing rotating and zooming the model by mouse.
 	threeControls = new OrbitControls( threeCamera, threeRenderer.domElement );
-	threeControls.target.set( 0, 0.5, 0 );
+	threeControls.target.set( 0, 0, 0 );
 	threeControls.update();
 	threeControls.enablePan = false;
 	threeControls.enableDamping = true;
@@ -69,12 +56,12 @@ function OnBeforeProjectStart(runtime)
 	// load a resource
 	loader.load(
 		// resource URL
-		'models/passive.obj',
+		'passive.obj',
 		// called when resource is loaded
 		function ( object ) {
 
 			// Insert the model to the scene and play its animation.
-			object.position.set(1, 1, 0);
+			object.position.set(0, 0, 0);
 			object.scale.set(.01, .01, .01);
 			threeScene.add( object );
 			threeMixer = new THREE.AnimationMixer(object);
@@ -90,20 +77,17 @@ function OnBeforeProjectStart(runtime)
 		// called when loading has errors
 		function ( error ) {
 
-			console.log( 'An error happened' + error.toString());
+			console.log( 'An error happened: ' + error.toString());
 
 		}
 	);
 
+	runtime.addEventListener('resize', e => OnResize(e));
 	
-	// Attach Construct event listeners for handling resize and rendering.
-	runtime.addEventListener("resize", e => OnResize(e));
-	
-	runtime.addEventListener("tick", () => OnTick(runtime));
+	runtime.addEventListener('tick', () => OnTick(runtime));
 }
 
 
-// In Construct's resize event, update the 3D renderer size to match.
 function OnResize(e)
 {
 	threeCamera.aspect = e.cssWidth / e.cssHeight;
@@ -112,9 +96,6 @@ function OnResize(e)
 	threeRenderer.setSize(e.cssWidth, e.cssHeight);
 }
 
-// In Construct's tick event, update the 3D animation playback and
-// 3D rendering. (Note this uses Construct's delta-time value rather
-// than THREE.Clock).
 function OnTick(runtime)
 {
 	threeMixer.update(runtime.dt);
